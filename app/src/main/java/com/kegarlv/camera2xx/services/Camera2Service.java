37ed7320300327c6ebf37a054081b9c6596c459d
Ivan Voloshyn <ivolos@softserveinc.com>
2022-01-17 16:03:59 +0200

    Implement Camera2 service

--- app/src/main/java/com/kegarlv/camera2xx/services/Camera2Service.java
+++ app/src/main/java/com/kegarlv/camera2xx/services/Camera2Service.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+import com.kegarlv.camera2xx.NativeLib;
 
 import com.kegarlv.camera2xx.ICamera2Api;
 import com.kegarlv.camera2xx.IGetCameraIdListCallback;
@@ -15,7 +16,17 @@ public class Camera2Service extends Service {
         return new ICamera2Api.Stub() {
             @Override
             public void getCameraIdList(IGetCameraIdListCallback cb) throws RemoteException {
-                cb.cameraIdListReady(null);
+                new NativeLib().getCameraIdList(new NativeLib.GetCameraIdListCallback() {
+                    @Override
+                    public void cameraIdListReady(String[] ids) {
+                        try {
+                            cb.cameraIdListReady(ids);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
             }
         };
     }
